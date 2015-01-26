ab8e0bcbc269fe7397a50689dd45c2b76a37c44c
Peter Maselkowski <pmaselkowski@gmail.com>
2015-01-26 11:33:29 +0100

    + Initial

---
+++ src/ZammModule.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * This SOFTWARE PRODUCT is protected by copyright laws and international copyright treaties,
+ * as well as other intellectual property laws and treaties.
+ * This SOFTWARE PRODUCT is licensed, not sold.
+ * For full licence agreement see enclosed LICENCE.html file.
+ *
+ * @licence LICENCE.html
+ * @copyright Copyright (c) Piotr Masełkowski <pmaselkowski@gmail.com>
+ * @copyright Copyright (c) Maslosoft
+ * @link http://maslosoft.com/
+ */
+
+namespace Maslosoft\Zamm;
+
+use Maslosoft\Ilmatar\Components\WebModule;
+
+/**
+ * MenulisModule
+ *
+ * @author Piotr Maselkowski <pmaselkowski at gmail.com>
+ */
+class ZammModule extends WebModule
+{
+
+	public function getBaseDir()
+	{
+		return realpath(__DIR__ . '/../../');
+	}
+
+}
