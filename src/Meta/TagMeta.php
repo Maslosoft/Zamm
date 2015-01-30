78cc4cf437667e6be448df9726a2f9969cbc9a5f
Peter Maselkowski <pmaselkowski@gmail.com>
2015-01-30 12:06:37 +0100

    * PHP docs

--- src/Meta/TagMeta.php
+++ src/Meta/TagMeta.php
@@ -8,20 +8,39 @@
 
 namespace Maslosoft\Zamm\Meta;
 
+use Maslosoft\Zamm\Decorators\ZammTags;
 use Maslosoft\Zamm\Renderers\IRenderer;
 
 /**
  * TagMeta
- *
+ * @see ZammTags
  * @author Piotr Maselkowski <pmaselkowski at gmail.com>
  */
 class TagMeta
 {
 
+	/**
+	 * Current working class name
+	 * @var string
+	 */
 	public $className = '';
-	public $name = '';
-	public $interfaces = '';
 
+	/**
+	 * Current method or property name
+	 * @var string
+	 */
+	public $name = '';
+
+	/**
+	 * Implemented renderer interfaces
+	 * @var string[]
+	 */
+	public $interfaces = [];
+
+	/**
+	 * Class constructor
+	 * @param IRenderer $renderer
+	 */
 	public function __construct(IRenderer $renderer)
 	{
 		$this->interfaces = class_implements($renderer);
@@ -29,6 +48,10 @@ class TagMeta
 		$this->name = $renderer->getName();
 	}
 
+	/**
+	 * Convert to json
+	 * @return string
+	 */
 	public function toJson()
 	{
 		return json_encode($this, JSON_FORCE_OBJECT);
