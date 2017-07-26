b6058e6dff52cf32cca62ef7a2e03a3e0e5eeb70
Peter Maselkowski <pmaselkowski@gmail.com>
2017-07-26 17:11:07 +0200

    * Docs

--- docs/quick-start/index.md.php
+++ docs/quick-start/index.md.php
@@ -1,40 +1,59 @@
 <?php
 
+use Maslosoft\Zamm\ApiUrl;
+use Maslosoft\Zamm\Capture;
 use Maslosoft\Zamm\Ignore;
+use Maslosoft\Zamm\ShortNamer;
 use Maslosoft\Zamm\Source;
 use Maslosoft\Zamm\Zamm;
 ?>
 <?php
+ShortNamer::defaults()->md();
 $zamm = new Zamm(Zamm::class);
 $ignore = new Zamm(Ignore::class);
 /* @var $ignore Ignore */
 $source = new Zamm(Source::class);
 /* @var $source Source */
+
+$namer = new ShortNamer(ShortNamer::class);
 ?>
 <title>1. Quick Start</title>
 # Zamm Quick Start Guide
 
-##<?= $zamm; ?>
+### API Links and naming
 
+Zamm can generate links to api made by [apigen][apigen]. To use
+this feature add API roots for specified namespaces:
 
-##<?= $source; ?>
-
-##<?= $ignore; ?>
-<?= $ignore::open(); ?>
-<?= $ignore::close(); ?>
-
-Example:
 <?php
-Ignore::open();
-Ignore::open();
-$object = new Zamm;
-Ignore::close();
-Ignore::close();
+Capture::open();
+ApiUrl::setRoot([
+        '/my-project/api/' => 'Maslosoft\\MyProject'
+]);
+echo Capture::close()->md();
 ?>
 
-This will result in plain php code in documentation:
+Then it can be used to create links to API of class,
+it's properties or methods using <?= $namer; ?>:
+
 <?php
-Ignore::open();
-$object = new Zamm;
-Ignore::close();
+Capture::open();
+$typeUrl = new ShortNamer(ApiUrl::class);
+// NOTE: setRoot is method of ApiUrl
+$methodUrl = (new ShortNamer(ApiUrl::class))->setRoot();
+echo Capture::close()->md();
 ?>
+
+Above will result in following links:
+
+> <?= $typeUrl; ?> and <?= $methodUrl; ?>.
+
+### Capturing code parts
+
+Code fragments embedded in documentation can be [captured][capture]
+with <?= new ShortNamer(Capture::class); ?>. This is will slice out
+file fragment and store it internally. Later this can be outputted
+as a code example. Thus resulting in valid PHP example code.
+
+[apigen]: https://github.com/ApiGen/ApiGen
+[capture]: ../capture/
