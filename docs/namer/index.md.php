9ac54c260646097e29ed0d9d7c63646cf77d2fd8
Peter Maselkowski <pmaselkowski@gmail.com>
2017-07-26 18:31:09 +0200

    * Docs

--- docs/namer/index.md.php
+++ docs/namer/index.md.php
@@ -5,12 +5,18 @@ use Maslosoft\Ilmatar\Components\Controller;
 use Faker\Factory as FF;
 use Maslosoft\Zamm\Capture;
 use Maslosoft\Zamm\Namer;
+use Maslosoft\Zamm\ShortNamer;
+use Maslosoft\Zamm\Zamm;
+
 ?>
 <?php
 /* @var $this Controller */
 /* @var $form ActiveForm */
 ?>
-
+<?php
+$sn = new ShortNamer(ShortNamer::class);
+$n = new ShortNamer(Namer::class);
+?>
 <title>4. Namer</title>
 #Namer
 
@@ -37,3 +43,63 @@ Capture::open();
 Namer::defaults()->md();
 echo Capture::close()->md;
 ?>
+
+## Short Namer
+
+Alternatively, there is also <?= $sn; ?> class, to create short
+class names - without namespace part. Fully qualified class name
+is only present on `title` attribute of link.
+
+Example of <?= $sn; ?> usage:
+
+<?php
+Capture::open();
+$name = (new ShortNamer(Capture::class));
+echo Capture::close()->md;
+?>
+
+This will result in short class name:
+
+> <?= $name; ?>
+
+
+Notice that no output type was specified, as specifying output
+type with <?= $n->method('defaults'); ?> will affect both <?= $n; ?>
+and <?= $sn; ?>;
+
+## Accessing Class Parts
+
+To get name of method or property, try to get or call it on namer instance.
+This will invoke PHP magic `__get` and `__call` respectivelly:
+
+<?php
+Capture::open();
+$name = (new ShortNamer(Zamm::class))->converters;
+echo Capture::close()->md;
+?>
+
+This will result in creating link for `converters` property:
+
+> <?= $name; ?>
+
+
+### Alternative Syntax
+
+In some cases method or property name might exists in namer class thus
+failing to invoke PHP Magic method. In such case, it is possible to
+use <?= $n->method('method'); ?> and
+<?= $n->method('property'); ?> directly:
+
+
+<?php
+Capture::open();
+$name = (new ShortNamer(Zamm::class))->property('converters');
+echo Capture::close()->md;
+?>
+
+This will result in link same as in previous example:
+
+> <?= $name; ?>
+
+
+
