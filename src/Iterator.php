92467be7d7c126aa3a4e5033959d36db4d392a5a
Peter Maselkowski <peter@maslosoft.com>
2017-10-11 15:27:56 +0200

    * sort methods by getters/setters first, and getters/setters in pairs

--- src/Iterator.php
+++ src/Iterator.php
@@ -76,7 +76,78 @@ class Iterator
 		{
 			$methods[] = $method->name;
 		}
-		sort($methods);
+
+		/**
+		 * Sort methods that:
+		 * * Getters and setters are first
+		 * * Getters and setters are side-by-side to each other counterpart
+		 */
+		$sort = function($a, $b)
+		{
+			// Whether is getter or setter
+			$isAGS = false;
+			$isBGS = false;
+			// Whether is getter
+			$isAG = false;
+			$isBG = false;
+			// Whether is setter
+			$isAS = false;
+			$isBS = false;
+			// Check if it's getter or setter
+			if (preg_match('~^[gs]et[A-Z0-9]~', $a))
+			{
+				if(preg_match('~^get[A-Z0-9]~', $a))
+				{
+					$isAG = true;
+				}
+				else
+				{
+					$isAS = true;
+				}
+				$a = substr($a, 3);
+				$isAGS = true;
+			}
+			// Check if it's getter or setter
+			if (preg_match('~^[gs]et[A-Z0-9]~', $b))
+			{
+				if(preg_match('~^get[A-Z0-9]~', $b))
+				{
+					$isBG = true;
+				}
+				else
+				{
+					$isBS = true;
+				}
+				$b = substr($b, 3);
+				$isBGS = true;
+			}
+			if($isAGS && !$isBGS)
+			{
+				return -1;
+			}
+			if(!$isAGS && $isBGS)
+			{
+				return 1;
+			}
+			if ($a == $b)
+			{
+				if($isAGS && $isBGS)
+				{
+					if($isAG && $isBS)
+					{
+						return -1;
+					}
+					if($isBG && $isAS)
+					{
+						return 1;
+					}
+				}
+				return 0;
+			}
+			return ($a < $b) ? -1 : 1;
+		};
+		usort($methods, $sort);
+
 		return $methods;
 	}
 
